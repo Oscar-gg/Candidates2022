402526fc9f3a12dd9442795ff58d41d0371db358
Oscar-gg <oscar.arreola.jr@gmail.com>
2022-09-15 19:47:52 -0500

    Archivos para documentacion y esqueleto del programa general agregados

---
+++ candidates_2022/candidates_2022.ino
@@ -0,0 +1,11 @@
+// Hacer esqueleto de lógica del robot.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ documentacion/documentacion.ino
@@ -0,0 +1,11 @@
+// Documentación de componentes individuales. Registro de cómo se usan
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
