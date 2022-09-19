10a2e87befca11bb3ac8257f593d178f94da4e6b
Oscar-gg <oscar.arreola.jr@gmail.com>
2022-09-18 22:53:19 -0500

    Backbone of setup and loop functions created.

--- candidates_2022/candidates_2022.ino
+++ candidates_2022/candidates_2022.ino
@@ -1,11 +1,86 @@
-// Hacer esqueleto de lógica del robot.
+// Esqueleto de lógica del robot.
+
+char zone = 'u';
+char color = 'u';
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  setPins();
+  attachObjects();
+  sensorData();
+  setZone();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorData();
+  
+  switch (zone){
+    case 'A':
+      zoneA();
+      break;
+    case 'B':
+      zoneB();
+      break;
+    case 'C':
+      zoneC();
+      break;
+  }
+}
 
+// Determines the next zone that the robot must finish. Only called on setup. Handles Lack of Progress. Causes infinite loop if no meaningful color is detected.
+// Color legend: each letter corresponds to the initial letter of the color, in English. Exception: case 'E'; 'P' (Pink) is already used (Purple).
+void setZone(){
+  switch(color){
+    case 'Y':
+      zone = 'A';
+      break;
+    case 'G':
+    case 'B':
+    case 'R':
+      zone = 'B';
+      break;
+    case 'P':
+      zone = 'C';
+      break;
+     case 'E': 
+      zone = 'E';
+      break;
+     default:
+      sensorData();
+      setZone();
+  }
+    
+}
+
+// Bind i/o pins.
+void setPins(){
+  
+}
+
+// set/attach objects such as motors.
+void attachObjects(){
+  
+}
+
+// Updates data recieved by all sensors.
+void sensorData(){
+  
+}
+
+// Function to make sure the robot advances in staight line. Pending: determine if magnetometer or ultrasonic sensor will be used.
+void calibrate(){
+  
+}
+
+void zoneA(){
+  
+}
+
+void zoneB(){
+  
+}
+
+void zoneC(){
+  
 }
